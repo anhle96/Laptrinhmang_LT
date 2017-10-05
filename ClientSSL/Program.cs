b3bbce687cfebc094b3cf4132083b84fa72475c0
unknown <lequocanh96@gmail.com>
2017-10-05 10:20:51 +0700

    Up bai tap LT

---
+++ ClientSSL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSSL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Khoi tao Client");
+            //b1 xac dinh IP Server
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            //b2 tao Socket
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //b3 gui nhan data
+            Console.WriteLine("Client gui data");
+
+
+
+
+
+
+            //b4 dong client socket
+            client.Close();
+        }
+    }
+}
---
+++ ServerSSL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerSSL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //b1 tao socket udp
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 12345);
+            //b2 ket noi Socket voi card mang
+            server.Bind(ip);
+            //b3 gui nhan data
+            //Server phai nhan data truoc
+            byte[] rec = new byte[25];
+            EndPoint client = (EndPoint)ip;
+            server.ReceiveFrom(rec, ref client);
+
+
+
+
+
+
+            //b4 dong server
+            server.Close();
+        }
+    }
+}
